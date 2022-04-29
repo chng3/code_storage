ee7185aea7779fde4c6cca56e6073546ef6e9505
Zhuang <chngdolphin@gmail.com>
2022-04-29 16:32:34 +0800

    Update practice.js
    完成泡泡老板的最后需求

--- practice.js
+++ practice.js
@@ -186,11 +186,26 @@ var bestSolutions = getBestResults(scores, highScore);
 //显示函数接受的形参(整形数组副本)中分数最高的所有索引(至少一个)
 console.log("Solution with highest score: " + bestSolutions);
 
-//待解决
-var getMostCostEffectiveSolution = funciton(scores, costs, highScore){
-	costs = 0;
-	getMostCostEffectiveSolution
-	for (var i = 0;i < scores.length; i++) {
-		
-	}
+// 本人简单暴力解决方式 实现得到返回最佳性价比配方的索引的函数
+// getMostCostEffectiveSolution函数得接受一对平行数组scores和costs,还有一个是scores数组中最高得分的数值
+var getMostCostEffectiveSolution = function(scores, highScore, costs){
+	var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            //向数组的最后一个位置添加元素“#+索引号” 使用数组对象中的push方法
+            bestSolutions.push(i);
+        }
+    }
+
+    //记录得分最高且成本最低的配方的索引-变量index
+    var index = bestSolutions[0];
+    var mostCostEffectivePrice = costs[bestSolutions[0]];
+    for (var i = 0; i < bestSolutions.length; i++) {
+    	if (mostCostEffectivePrice > costs[bestSolutions[i]]) {
+    		index = bestSolutions[i];
+    	}
+    };
+    return index;
 };
+
+console.log("最具性价比的配方是: #" + getMostCostEffectiveSolution(scores, highScore, costs));
