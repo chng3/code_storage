30ecd0b2ed0d829a5f42ab53fc7bbceb8c29800f
chngdolphin <chngdolphin@gmail.com>
2022-05-08 22:28:48 +0800

    更新仓库

--- LanQiaoCup/CountMounTainNumber/script.js
+++ LanQiaoCup/CountMounTainNumber/script.js
@@ -1,5 +1,6 @@
 // mtain.js
 
+//显示输入的区间
 function showRange() {
     var leftRange = document.getElementById("lNumberTxt").value;
     var rightRange = document.getElementById("rNumberTxt").value;
@@ -8,6 +9,7 @@ function showRange() {
 
 }
 
+//右边区间超过范围提示
 function showRtRangeTip(){
     var rnumber = parseInt(document.getElementById("rNumberTxt").value);
     if(rnumber>2022000){
@@ -15,12 +17,14 @@ function showRtRangeTip(){
     }
 }
 
+//计算
 function goGo() {
     var lNumber = parseInt(document.getElementById("lNumberTxt").value);
     var rNumber = parseInt(document.getElementById("rNumberTxt").value);
     countMT(lNumber,rNumber);
 }
 
+//验证数字总函数
 function countMT(num1, num2) {
     var num = 0;
     for (var i = num1; i <= num2; i++) {
@@ -32,14 +36,19 @@ function countMT(num1, num2) {
     alert(num);
 }
 
+//验证数字条件是否满足
 function isMT(number) {
     //将number输出到字符串
     var str = number.toString();
+
+    //验证条件一:是否先单调不减
     for (var i = 1; i < (str.length + 1) / 2; i++) {
         if (str[i] < str[i - 1]) {
             return false;
         }
     }
+
+    //验证条件二:是否对称,利用双指针
     for (var i = 0; i < str.length / 2; i++) {
         if (str[i] != str[str.length - 1 - i]) {
             return false;
