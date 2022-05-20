94b73d64ae867fe2e32e557613f69973031e3c94
chngdolphin <chngdolphin@gmail.com>
2022-05-21 00:28:23 +0800

    使用JS内置对象window

--- HeadFirstJavaScript/First-To-Know-DOM/code.js
+++ HeadFirstJavaScript/First-To-Know-DOM/code.js
@@ -1,4 +1,13 @@
-var access = document.getElementById("code9");
-var code = access.innerHTML;
-code = code + " midnight";
-alert(code);
+function init() {
+    var access01 = document.getElementById("code9");
+    var access02 = document.getElementById("code7");
+    var code01 = access01.innerHTML;
+    var code02 = access02.innerHTML;
+    code01 = code01 + " midnight";
+    code02 = code02 + " coffee";
+    access01.innerHTML = code01;
+    access02.innerHTML = code02;
+}
+
+window.onload = init;
+
