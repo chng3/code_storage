ad857454040cda5865dd26a2e7c0bed99201ca46
chngdolphin <chngdolphin@gmail.com>
2022-05-21 13:24:01 +0800

    add

---
+++ HeadFirstJavaScript/To-Know-Object/chevy.js
@@ -0,0 +1,28 @@
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
+
+var cadi  = {
+    make: "通用",
+    passengers: 5,
+    covnertible: false,
+    mileage: 12892,
+    year: 1955
+    color: "棕褐色"
+};
+
+var fiat = {
+    meke: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passengers: 2,
+    convertible: false,
+    mileage: 88000
+};
