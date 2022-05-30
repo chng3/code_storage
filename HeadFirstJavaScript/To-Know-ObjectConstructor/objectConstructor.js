d8c8a4bb2cc00751b7cd0ded1467a31f96385dde
chngdolphin <chngdolphin@gmail.com>
2022-05-31 00:11:20 +0800

    init one Object constructor

--- HeadFirstJavaScript/To-Know-ObjectConstructor/objectConstructor.js
+++ HeadFirstJavaScript/To-Know-ObjectConstructor/objectConstructor.js
@@ -1,1 +1,9 @@
-var cadiParams = {make: "GM", model: "Cadillac", year: 1955, color: "tan", passengers: 5, convertible: false, mileage: 12892};
+let cadiParams = {
+    make: "GM", model: "Cadillac", year: 1955,
+    color: "tan", passengers: 5, convertible: false, mileage: 12892
+};
+
+let cadi = new Car(cadiParams);
+cadi.chrome = true;
+delete cadi.covnertible;
+
