20e39144e4528ba4c07b4ce5fb65ea7934a1c167
dolphinchng <chngdolphin@gmail.com>
2022-08-01 14:43:11 +0800

    fix syntax error

--- HeadFirstJavaScript/To-Know-function-deeply/嵌套对作用域的影响/test.js
+++ HeadFirstJavaScript/To-Know-function-deeply/嵌套对作用域的影响/test.js
@@ -1,16 +1,3 @@
-// var secret = "007";
-
-// function getSecret() {
-//     var secret = "008";
-
-//     function getValue() {
-//         return secret;
-//     }
-//     return getValue();
-// }
-
-// console.log(getSecret());
-
 var secret = "007";
 
 function getSecret() {
@@ -19,8 +6,24 @@ function getSecret() {
     function getValue() {
         return secret;
     }
+    // 返回的是运行getValue函数后所返回的结果
     return getValue();
 }
 
-var getValueFun = getSecret();
-getValueFun();
+console.log(getSecret());
+// 控制台打出 "008"
+
+// var secret = "007";
+
+// function getSecret() {
+//     var secret = "008";
+
+//     function getValue() {
+//         return secret;
+//     }
+//     // 返回一个函数引用
+//     return getValue;
+// }
+
+// var getValueFun = getSecret();
+// console.log(getValueFun()) // 控制台打出 "008"
