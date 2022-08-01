89015af4c9fe0f3cb534c79e6b7a8d3e16a8086c
dolphinchng <chngdolphin@gmail.com>
2022-08-01 12:18:29 +0800

    take a break

---
+++ HeadFirstJavaScript/To-Know-function-deeply/嵌套对作用域的影响/test.js
@@ -0,0 +1,26 @@
+// var secret = "007";
+
+// function getSecret() {
+//     var secret = "008";
+
+//     function getValue() {
+//         return secret;
+//     }
+//     return getValue();
+// }
+
+// console.log(getSecret());
+
+var secret = "007";
+
+function getSecret() {
+    var secret = "008";
+
+    function getValue() {
+        return secret;
+    }
+    return getValue();
+}
+
+var getValueFun = getSecret();
+getValueFun();
