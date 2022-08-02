cf11caa69ba762c5a94246bf71282bf80b9abbf2
dolphinchng <chngdolphin@gmail.com>
2022-08-03 00:58:29 +0800

    继续完善闭包总结文档

--- HeadFirstJavaScript/To-Know-Closures/使用闭包实现计数器.js
+++ HeadFirstJavaScript/To-Know-Closures/使用闭包实现计数器.js
@@ -2,13 +2,12 @@
 function makeCounter() {
     let count  = 0;
     function counter() {
-        count+=1;       // count 为自由函数
+        count += 1;       // count 为自由函数
         return count;
     }
     return counter;     // 构成闭包，在其环境中存储了变量count
     
 }
-// TODO: 将此文件代码替换闭包总结文档中的 “实践中的闭包“片段
 // 调用 makeCounter 时，我们获得的是一个闭包：一个函数及其环境
 let doCounter = makeCounter();
 console.log(doCounter());
