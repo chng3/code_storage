98d01d60191e673b848573ecb15ab91b68511662
dolphinchng <chngdolphin@gmail.com>
2022-09-14 11:46:39 +0800

    修改路径

---
+++ HeadFirstJavaScript/To-Know-Prototype/ceshi.js
@@ -0,0 +1,9 @@
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+}
+
+let Fido = new Dog("Fido", "Mixed", 38);
+let Fluffy = new Dog("Fluffy", "Poodle", 30);
+let Spot = new Dog("Spot", "Chihuahua", 10);
