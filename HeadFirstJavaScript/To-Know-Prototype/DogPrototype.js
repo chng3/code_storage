8e82c38819caa333f87d2ed250c9eee81e9a8b14
dolphinchng <chngdolphin@gmail.com>
2022-09-15 23:24:13 +0800

    take a break

--- HeadFirstJavaScript/To-Know-Prototype/DogPrototype.js
+++ HeadFirstJavaScript/To-Know-Prototype/DogPrototype.js
@@ -47,8 +47,35 @@ spot.wag();
 
 let barnaby = new Dog("Barnaby", "Basset Hound", 55);
 
+Dog.prototype.sitting = false;
+
 Dog.prototype.sit = function () {
-    console.log(this.name + " is now sitting");
+    if(this.sitting){
+        console.log(this.name + " is already sitting");
+    } else {
+        // 这里的 this 指向原始对象，即方法被调用的对象。第一次调用时，默认给小狗实例添加了属性sitting的值，便于后续独立跟踪自己是否是坐着的
+        this.sitting = true;
+        console.log(this.name + " is now sitting");
+    }
+    
 }
 
+// 小狗实例刚创建时，从原型那里继承了属性sitting， 该属性的值默认为false;但调用方法sit后，
+// 就给小狗实例添加了属性sitting的 值，导致在小狗实例中创建了属性sitting。这让我们能够给所
+// 有小狗对象指定默 认值，并在需要时对各个小狗进行定制。
+
+
+console.log(spot.hasOwnProperty("sitting")); 
+
 barnaby.sit();
+barnaby.sit();
+spot.sit();
+spot.sit();
+
+
+// 判断使用的属性包含在实例还是原型中用 hasOwnProperty 方法，传入参数为字符串形式的属性名
+
+console.log(spot.hasOwnProperty("species"));
+console.log(fido.hasOwnProperty("species"));
+console.log(spot.hasOwnProperty("sitting"));
+console.log(fido.hasOwnProperty("sitting"));
--- HeadFirstJavaScript/To-Know-Prototype/RobotPrototype.js
+++ HeadFirstJavaScript/To-Know-Prototype/RobotPrototype.js
@@ -7,15 +7,18 @@ function Robot(name, year, owner) {
 Robot.prototype.maker = "ObjectsRUs";
 
 Robot.prototype.speak = function () {
-    console.log("I'm a robot");
+    // alert("Warning warning!!");
+    console.log("Warning warning!!");
 }
 
 Robot.prototype.makeCoffee = function () {
-    console.log("I'm goning to the way to drinking coffee!");
+    // alert("Making coffee");
+    console.log("Making coffee");
 }
 
 Robot.prototype.blinkLights = function () {
-    console.log("B ling B ling B ling!");
+    // alert("Blink Blink!");
+    console.log("Blink Blink!");
 }
 
 let robby = new Robot("Robby", 1956, "Dr.Morbius");
@@ -23,20 +26,24 @@ let rosie = new Robot("Rosie", 1962, "George Jetson");
 
 robby.onOffSwitch = true;
 robby.makeCoffee = function () {
+    // alert("Fetching a coffee from Starbucks.")
+    // alert("I'm on the road to drinking coffee in Starbuck!");
     console.log("I'm on the road to drinking coffee in Starbuck!");
 }
 
 rosie.cleanHouse = function () {
-    console.log("I'm cleaning the room!");
+    // alert("Cleaning! Spic and Span soon...")
+    // alert("I'm cleaning the room!");
+    console.log("Cleaning! Spic and Span soon...")
 }
 
-console.log(robby.name + " was made by " + robby.maker + 
-            " in " + robby.year + " and is owned by " + robby.owner);
+console.log(robby.name + " was made by " + robby.maker +
+    " in " + robby.year + " and is owned by " + robby.owner);
 robby.makeCoffee();
 robby.blinkLights();
 
-console.log(rosie.name + " was made by " + rosie.maker + 
-            " in " + rosie.year + " and is owned by " + rosie.owner);
+console.log(rosie.name + " was made by " + rosie.maker +
+    " in " + rosie.year + " and is owned by " + rosie.owner);
 
 rosie.cleanHouse();
 
