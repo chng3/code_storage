14717fcd7dc27aea6995e73a724f731d77387019
dolphinchng <chngdolphin@gmail.com>
2022-09-19 20:21:04 +0800

    FIXED pN is

--- JsRegularExpressPractice/all_RegEx_Storage.js
+++ JsRegularExpressPractice/all_RegEx_Storage.js
@@ -1,6 +1,10 @@
 //examle.js
 // 创建正则表达式，可调用构造函数RegExp，并传入放在两个斜杠之间的搜索模式
 
+// TODO: 地区码 待验证
 var areaCode = new RegExp(/[0-9]{3}/);
 
-var phoneNumber = new RegExp(/^\d{3}-?\d{4}$/);
+// 符合大陆以1开头的11位手机号码，具体号段不做限制
+var phoneNumber = new RegExp(/^1\d{10}$/);
+
+
