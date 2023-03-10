6edf2112a6966c882e1b09a47f72bb0c0badbc28
Zhuang <chngdolphin@gmail.com>
2023-03-10 23:31:34 +0800

    FINISHED miniGameBattleShip

--- HeadFirstJavaScript/To-Know-Type/undefined.js
+++ HeadFirstJavaScript/To-Know-Type/undefined.js
@@ -1,5 +1,18 @@
+// 使用未初始化的变量，将会遇到undefined；可以通过 undefined 判断是否给变量赋值了
 let x;
 if( x == undefined){
+    // x 未定义！处理这种情况！
     console.log("x is : " + x);
     // show" x is : undefined" in console
 }
+
+let customer = {
+    name: "Jenny"
+};
+if(customer.phoneNumber == undefined) {
+    // 获取客户的电话号码
+    console.log("the customer's phone number isn't definded yet");
+}
+
+let test1 = Math.sqrt(-9);
+console.log(isNaN(test1));
